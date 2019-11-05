1635874bf04432e0ddc4612aa731ad31f0731193
gopiprasanthp <gopiprasanthp@virtusa.com>
2019-11-05 19:27:05 +0530

    naive bayes text classification.

--- textclassification1.py
+++ textclassification1.py
@@ -11,7 +11,7 @@ documents=[]
 for category in movie_reviews.categories():
     
     for fileid in movie_reviews.fileids(category):
-        documents.append([list(movie_reviews.words(fileid)),category])
+        documents.append((list(movie_reviews.words(fileid)),category))
         
 random.shuffle(documents)
 
@@ -36,8 +36,18 @@ def get_Features(document):
     words=set(document)
     features={}
     for w in words:
-        features[w]={w in word_features}
+        features[w]=(w in word_features)
     return features;
 
 featuresets=[(get_Features(rev),category) for (rev,category) in documents ]
-        
+
+#text classification with naive bayes classifier
+
+train_set=featuresets[:1900]
+test_set=featuresets[1900:]
+
+naive_classifier=nltk.NaiveBayesClassifier.train(train_set)
+
+print("Accuracy of Model : " , nltk.classify.accuracy(naive_classifier,test_set)*100)        
+
+print(naive_classifier.show_most_informative_features(15))
